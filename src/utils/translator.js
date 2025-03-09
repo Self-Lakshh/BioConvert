64a93db4be37385480ef5c4b681a086383c70ec4
Self-Lakshh <lakshyachopra2004@gmail.com>
2025-03-09 14:00:00 +0530

    added codon table and translation functions

--- src/utils/translator.js
+++ src/utils/translator.js
@@ -11,3 +11,34 @@
 
 export const dnaToRna = (dna) => dna.toUpperCase().replace(/T/g, "U");
 export const rnaToDna = (rna) => rna.toUpperCase().replace(/U/g, "T");
+
+const codonTable = {
+    "UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L",
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
+    "UAU": "Y", "UAC": "Y", "UAA": "-", "UAG": "-",
+    "UGU": "C", "UGC": "C", "UGA": "-", "UGG": "W",
+    "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+    "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K",
+    "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"
+};
+
+export const rnaToProtein = (rna) => {
+    let protein = "";
+    const rnaUpper = rna.toUpperCase().replace(/[^ACGU]/g, "");
+    for (let i = 0; i < rnaUpper.length; i += 3) {
+        const codon = rnaUpper.substring(i, i + 3);
+        protein += codonTable[codon] || "?";
+    }
+    return protein;
+};
+
+export const dnaToProtein = (dna) => rnaToProtein(dnaToRna(dna));

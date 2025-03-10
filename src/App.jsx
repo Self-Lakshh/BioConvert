c4d079c28f9f623e007d4e1f10d325d21ae93b97
Self-Lakshh <lakshyachopra2004@gmail.com>
2025-03-10 15:00:00 +0530

    created basic app structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+﻿import { useState } from "react";
+const App = () => {
+  const [result, setResult] = useState("");
+  return (
+    <div>
+      <h1>BioConvert</h1>
+      <p>Bioinformatics Sequence Translator</p>
+    </div>
+  );
+};
+export default App;

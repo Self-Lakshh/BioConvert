13860b4505c0052f5f115aa8751cb0ec792e0830
Self-Lakshh <lakshyachopra2004@gmail.com>
2025-03-12 11:15:00 +0530

    designed input component layout

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,11 @@
+﻿import { useState } from "react";
+const InputBox = ({ setResult }) => {
+  const [input, setInput] = useState("");
+  return (
+    <div>
+      <label>Enter Sequence</label>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+    </div>
+  );
+};
+export default InputBox;

cd8ee0d3c9d83d29f88d59410e0a41fd7faa60f8
Lakshh Chopra <lakshyachopra2004@gmail.com>
2025-03-19 04:27:26 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
             <Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                 <Box
                     component="img"
-                    src="https://cdn.discordapp.com/attachments/1203219990203858955/1351688943279804538/bioconvertlogo.jpg?ex=67db49ed&is=67d9f86d&hm=7062198470e9ab2f811ce4299af0151d336277201818d8c1dc9a29abbd1d0063&"
+                    src="src\assets\bioconvertlogo.jpg"
                     alt="Logo"
                     sx={{ height: { xs: "1.5rem", sm: "2rem" }, marginRight: "0.5rem" }}
                 />
